711a8563ac1abacc240f94880094b3615896ca11
W. S. Wellington <thewellington@gmail.com>
2014-03-07 08:24:39 -0800

    updating some comments

--- master-setup.sh
+++ master-setup.sh
@@ -2,14 +2,14 @@
 #
 #
 
-# lets set some variables
+# let's set some variables
 PUPPET_URL="https://s3.amazonaws.com/pe-builds/released/3.2.0"
 TARBALL='puppet-enterprise-3.2.0-el-6-x86_64.tar.gz'
 DOWNLOAD_DIR='/root/Downloads'
 DEST_DIR='puppet-enterprise-3.2.0-el-6-x86_64'
 
 
-# okay, lets get to work!
+# okay, let's get to work!
 curl "${PUPPET_URL}/${TARBALL}" -o "${DOWNLOAD_DIR}/${TARBALL}"
 
 tar -xzf "${DOWNLOAD_DIR}/${TARBALL}" -C "${DOWNLOAD_DIR}"
